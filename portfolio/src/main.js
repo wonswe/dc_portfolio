0d77ff3c7f35e52471f2c245fd26bff38b2af580
woncodes <64626054+woncodes@users.noreply.github.com>
2025-07-07 20:25:07 -0400

    JS-Home

--- portfolio/src/main.js
+++ portfolio/src/main.js
@@ -8,3 +8,10 @@ document.addEventListener('scroll', () => {
     header.classList.remove('header--dark');
   }
 });
+
+// Home 섹션을 아래로 스크롤시 점진적으로 투명도를 적용함 (1에서 0으로)
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', () => {
+  home.style.opacity = 1 - window.scrollY / homeHeight;
+});
