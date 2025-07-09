27144853bde600e1c4bab4e5c26e6dff7791942f
woncodes <64626054+woncodes@users.noreply.github.com>
2025-07-08 22:14:33 -0400

    JS-Projects

--- portfolio/src/projects.js
+++ portfolio/src/projects.js
@@ -1,1 +1,38 @@
 'use strict';
+
+// 프로젝트 필터링 관련 로직 처리
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('.projects');
+
+categories.addEventListener('click', (event) => {
+  const filter = event.target.dataset.category;
+  if (filter == null) {
+    return; // 함수 종료
+  }
+
+  handleActiveSelection(event.target);
+  filterProjects(filter);
+});
+
+function handleActiveSelection(target) {
+  const active = document.querySelector('.category--selected');
+  active.classList.remove('category--selected');
+  target.classList.add('category--selected');
+}
+
+function filterProjects(filter) {
+  projectsContainer.classList.add('anim-out');
+  projects.forEach((project) => {
+    console.log(project.dataset.type);
+    if (filter === 'all' || filter === project.dataset.type) {
+      project.style.display = 'block';
+    } else {
+      project.style.display = 'none';
+    }
+  });
+
+  setTimeout(() => {
+    projectsContainer.classList.remove('anim-out');
+  }, 250);
+}
